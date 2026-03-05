1b0bb4985dda044e3d5e9e360d01abdc9979fa9b
Roman <118828810+r0masaN@users.noreply.github.com>
2026-03-05 18:30:54 +0300

    created my_function.hpp

---
+++ include/my_function.hpp
@@ -0,0 +1,44 @@
+#ifndef MY_FUNCTION_HPP
+#define MY_FUNCTION_HPP
+
+template<typename T>
+class my_function;
+
+template<typename R, typename... Args>
+class my_function<R(Args...)> {
+    void *obj{nullptr};
+    void *(*copier)(void *){nullptr};
+    void (*deleter)(void *){nullptr};
+    R (*invoker)(void *, Args&&...){nullptr};
+
+    template<typename D>
+    static void *copy(void *obj) noexcept(noexcept(std::is_nothrow_copy_constructible_v<D>));
+
+    template<typename D>
+    static void destruct(void *obj) noexcept(noexcept(std::is_nothrow_destructible_v<D>));
+
+    template<typename D>
+    static R invoke(void *obj, Args&&... args);
+
+public:
+    my_function() = default;
+
+    template<typename L, typename D = std::decay_t<L>, typename = std::enable_if_t<std::is_invocable_r_v<R, D&, Args...>>>
+    my_function(L&& l) noexcept(noexcept(D(std::forward<L>(l))));
+
+    my_function(const my_function& other) noexcept(noexcept((*other.copier)(obj)));
+
+    my_function(my_function&& other) noexcept;
+
+    ~my_function() noexcept(noexcept(deleter(obj)));
+
+    my_function& operator=(const my_function& other) noexcept(noexcept((*deleter)(obj)) && noexcept(noexcept((*copier)(obj))));
+
+    my_function& operator=(my_function&& other) noexcept;
+
+    R operator()(Args&&... args) const;
+};
+
+#include "my_function.tcc"
+
+#endif
